3efa00ca5b937d3cfd4654bd944ff95746564310
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-24 23:11:27 +0100

    fetch countries

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}fields=name,capital,population,languages,flags`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+      console.log(error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import './css/styles.css';
+import { Notify } from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+import { debounce } from 'lodash';
 
 const DEBOUNCE_DELAY = 300;
-const searchBox = document.querySelector('#search-box');
+const searchBox = document.querySelector('input');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-// searchBox.addEventListener( "input", () => {
-
-// })
-// fetch("https://restcountries.com/v3.1/name/{name}").then(response) => {
-//     if(!response.ok) {
-
-//     }
-// }
+searchBox.addEventListener(
+  'input',
+  debounce(() => {
+    const search = searchBox.value;
+    console.log('input:', search);
+    if (search.length < 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+  }, DEBOUNCE_DELAY)
+);
