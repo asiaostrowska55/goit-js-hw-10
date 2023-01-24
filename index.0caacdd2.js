f84477e00ff0fdb1a7a51f79440923120bb8995a
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-24 22:11:59 +0000

    Deploying to gh-pages from @ asiaostrowska55/goit-js-hw-10@3efa00ca5b937d3cfd4654bd944ff95746564310 🚀

--- index.0caacdd2.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.0caacdd2.js.map
--- index.2a28e308.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.2a28e308.js.map
