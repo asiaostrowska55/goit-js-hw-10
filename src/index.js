08e6c75209783facb2921e894b9b411fb65cd0f0
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-24 20:50:50 +0100

    prepare

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// searchBox.addEventListener( "input", () => {
+
+// })
+// fetch("https://restcountries.com/v3.1/name/{name}").then(response) => {
+//     if(!response.ok) {
+
+//     }
+// }
