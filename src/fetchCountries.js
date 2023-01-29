de6378beef8555bcb516917a885e5825dc5b3f5b
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-29 12:00:44 +0100

    fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -5,7 +5,7 @@ export function fetchCountries(name) {
     'Access-Control-Allow-Headers': '*',
   });
   return fetch(
-    '`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`'
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
