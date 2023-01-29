801ac41d324b7b73e6937932986cca92f653fd70
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-29 16:01:34 +0100

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,11 @@
 export function fetchCountries(name) {
-  let headers = new Headers({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
-    'Access-Control-Allow-Headers': '*',
-  });
+  // let headers = new Headers({
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+  // });
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+    { mode: 'cors' }
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
